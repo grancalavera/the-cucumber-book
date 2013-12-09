7f4468dd89c06a243cbe5595b649eea74e322d7f
Leon Coto <leoncoto@gmail.com>
2013-12-08 21:19:53 -0600

    Progress on async code

--- projects/atm/features/support/hooks.rb
+++ projects/atm/features/support/hooks.rb
@@ -1,13 +1,4 @@
-# Before do |scenario|
-#   puts "Starting scenario #{scenario.name}"
-# end
-
-# After do |scenario|
-#   puts "Oh dear" if scenario.failed?
-# end
-
-# Around do |scenario, block|
-#   puts "About to run #{scenario.name}"
-#   block.call
-#   puts "Finished running #{scenario.name}"
-# end
+Before do
+  BalanceStore.new.balance = 0
+  TransactionQueue.clear
+end
