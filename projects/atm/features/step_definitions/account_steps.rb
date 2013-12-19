e7fbf08a7007d88638072830ed066aaf210e659f
Leon Coto <leoncoto@gmail.com>
2013-12-19 20:57:15 +0000

    Database initial work.

--- projects/atm/features/step_definitions/account_steps.rb
+++ projects/atm/features/step_definitions/account_steps.rb
@@ -3,6 +3,8 @@ Given /^my account has been credited with (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
 end
 
 And /^the balance of my account should be (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
-  my_account.balance.should eq(amount),
-    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+  eventually {
+    my_account.balance.should eq(amount),
+      "Expected the balance to be #{amount} but it was #{my_account.balance}"
+  }
 end
--- projects/atm/lib/transaction_processor.rb
+++ projects/atm/lib/transaction_processor.rb
@@ -1,15 +1,5 @@
 require_relative 'transaction_queue'
-require_relative 'balance_store'
+require_relative 'account'
 
 transaction_queue = TransactionQueue.new
-balance_store = BalanceStore.new
 puts "transaction processor ready"
-
-loop do
-  transaction_queue.read do |message|
-    sleep 1
-    transaction_amount = message.to_i
-    new_balance = balance_store.balance + transaction_amount
-    balance_store.balance = new_balance
-  end
-end
